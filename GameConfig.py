7b7101f460d8cbb7ae9278f881b32960569caa5e
rikirolly <riccardo.rolando@gmail.com>
2018-12-03 09:28:00 +0100

    Update GameConfig.py

--- GameConfig.py
+++ GameConfig.py
@@ -1,5 +1,8 @@
-BOARD_HEIGHT = 5
-BOARD_WIDTH = 5
-SHIP_A = {'mark': '@', 'length': 2}
-SHIP_B = {'mark': '#', 'length': 2}
-SHIPS = [SHIP_A, SHIP_B]
+BOARD_HEIGHT = 10
+BOARD_WIDTH = 10
+SHIP_A = {'mark': 'A', 'length': 2}
+SHIP_B = {'mark': 'B', 'length': 3}
+SHIP_C = {'mark': 'C', 'length': 3}
+SHIP_D = {'mark': 'D', 'length': 4}
+SHIP_E = {'mark': 'E', 'length': 5}
+SHIPS = [SHIP_A, SHIP_B, SHIP_C, SHIP_D, SHIP_E]
